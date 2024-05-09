5b437e418a2d62f5e0ec5f3e150fbb0d14cfadea
apple <xsherron126@gmail.com>
2024-05-10 10:50:18 +1200

    commit message

--- ISCG7420_Ass1/settings.py
+++ ISCG7420_Ass1/settings.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'ISCG7420_Ass1.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'UFViRS1gxl2M',
+        'HOST': 'ep-super-night-a4da0q8u.us-east-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
 
